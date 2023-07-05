b0b548173b5034ffd106fc1d925c726e9932b018
Ramon Almeida <ramongoncalves76@gmail.com>
2023-07-05 11:54:00 -0300

    feat: fix arrays

--- scripts-certbot/certbot-renew.sh
+++ scripts-certbot/certbot-renew.sh
@@ -46,77 +46,63 @@ if ! [[ $CRITICAL_PERIOD =~ $RE_PATTERN_NUMBER ]]; then
 fi
 
 
+calc_downtime_cert(){
+    local _cert_date_end=$(date --date="$(openssl x509 -in $LETS_PATH/live/$WORK_DIR/$FILE_NAME -text -noout | grep 'Not After' | cut -c 25-)" +%s)
+    
+    CERT_DOWNTIME=$(( ($_cert_date_end - $date_today) / $second_per_day ))
+}
+
+get_domains_cert(){
+    CERT_GEN_DOMAINS=($(openssl x509 -in $LETS_PATH/live/$WORK_DIR/$FILE_NAME -text -noout | grep -oP '(?<=DNS:)[^,]*' | tr "\n" " "))
+}
+
+renew_cert(){
+    docker-compose -f $DOCKER_COMPOSE_PATH run --rm app \
+    certonly --webroot --webroot-path=/var/www/certbot \
+    -m $CERT_EMAIL -d $CERT_DOMAINS_LIST \
+    --force-renewal --agree-tos
+}
+
 
 second_per_day=$[24*3600]
 date_today=$(date -d "now" +%s)
-
-fun_calc_downtime_cert(){
-    local _cert_date_end=$(date --date="$(openssl x509 -in $LETS_PATH/live/$WORK_DIR/$FILE_NAME -text -noout | grep 'Not After' | cut -c 25-)" +%s)
-    
-    CERT_DOWNTIME=$[($_cert_date_end - $date_today) / $second_per_day]
-}
-
-fun_get_domains_cert(){
-    cert_domains=$(openssl x509 -in $LETS_PATH/live/$WORK_DIR/$FILE_NAME -text -noout | grep -oP '(?<=DNS:)[^,]*' | tr "\n" " ")
-}
-
-fun_renew_cert(){
-    docker-compose -f $DOCKER_COMPOSE_PATH run --rm app \
-        certonly --webroot --webroot-path=/var/www/certbot \
-        -m $CERT_EMAIL -d $CERT_DOMAINS_LIST \
-        --force-renewal --agree-tos
-}
-
-    
-echo "|-+ Work Dir.: $WORK_DIR"
-echo "|-+ Filename.: $FILE_NAME"
-echo "|-+ Date.: $(date)"
-echo "|"
-echo "|-+ Conf. Domain.:"
-echo "  $CERT_DOMAINS_LIST"
-echo "|-+ Conf. Critical Period.:"
-echo "  downtime <= $CRITICAL_PERIOD"
-echo "|"
-echo "|+- Status:"
-echo "|" 
-
 LOG_REALTIME = ""
 
 if ! [[ -f $LETS_PATH/live/$CERT_WORKDIR/$FILE_NAME ]]; then
-    $LOG_REALTIME += "'$LETS_PATH/live/$CERT_WORKDIR/$FILE_NAME' not found, creating... "
+    LOG_REALTIME+="'$LETS_PATH/live/$CERT_WORKDIR/$FILE_NAME' not found, creating... "
 else
-    fun_calc_downtime_cert
-    $LOG_REALTIME += "'$LETS_PATH/live/$CERT_WORKDIR/$FILE_NAME' found, certificate downtime in '$CERT_DOWNTIME days'... CRITICAL_PERIOD=$CRITICAL_PERIOD, "
-
+    calc_downtime_cert
+    
+    LOG_REALTIME+="'$LETS_PATH/live/$CERT_WORKDIR/$FILE_NAME' found, certificate downtime in '$CERT_DOWNTIME days'... CRITICAL_PERIOD=$CRITICAL_PERIOD, "
+    
     if [[ $CERT_DOWNTIME -gt $CRITICAL_PERIOD ]]; then
-        $LOG_REALTIME += "nothing to do, the certificate is within the security space."
+        LOG_REALTIME+="nothing to do, the certificate is within the security space."
+        
+        echo $LOG_REALTIME >> $LOG_FILE
+        
         exit 0
     fi
 fi
 
-
-fun_renew_cert
-
-fun_calc_downtime_cert
+renew_cert
+calc_downtime_cert
 
 if [[ $cert_downtime -le $CRITICAL_PERIOD ]]; then
-    $LOG_REALTIME += "unsuccessful renewal."
+    LOG_REALTIME+="unsuccessful renewal."
+    
+    echo $LOG_REALTIME >> $LOG_FILE
+    
     exit 1
 fi
 
-fun_get_domains_cert
+get_domains_cert
+CERT_GEN_DOMAINS
 
-tmp_domains="$CERT_DOMAINS_LIST,"
+DOMAINS_LIST_TMP=$(echo $CERT_DOMAINS_LIST | tr "," " ")
 
-tmp_domains_arr=()
-while [[ $tmp_domains ]]; do
-    tmp_domains_arr+=( "${tmp_domains%%","*}" )
-    tmp_domains=${tmp_domains#*","}
-done
-
-unverified_domains=$(
-    for domain in ${tmp_domains_arr[@]}; do
-        [[ "$cert_domains" =~ "$domain" ]] || echo $domain
+UNVERIFIED_DOMAINS=$(
+    for domain in ${DOMAINS_LIST_TMP[@]}; do
+        [[ $domain =~ "$CERT_GEN_DOMAINS" ]] || echo $domain
     done
 )
 
